9498616a35cbce8973f5a43ae8047837c62db761
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-04-20 22:13:23 +0700

    * Split player movement into a component + Add bullet object

---
+++ Assets/Scripts/Components/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float turnSpeed = 1.0f;
+
+    private bool isThrusting = false;
+    private float turnDirection;
+
+    void Update()
+    {
+        isThrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            turnDirection = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnDirection = -1f;
+        }
+        else
+        {
+            turnDirection = 0f;
+        }
+    }
+
+	void FixedUpdate()
+	{
+        if (isThrusting)
+        {
+            rb.AddForce(transform.up * moveSpeed);
+        }
+
+        if (turnDirection != 0f)
+        {
+            rb.AddTorque(turnDirection * turnSpeed);
+        }
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,49 +1,13 @@
 using NUnit.Framework;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1.0f;
-    [SerializeField] private float turnSpeed = 1.0f;
-
-    private Rigidbody2D rb;
-
-    private bool isThrusting = false;
-    private float turnDirection;
+    private PlayerMovement playerMovement;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
-
-    void Update()
-    {
-        isThrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            turnDirection = 1f;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            turnDirection = -1f;
-        }
-        else
-        {
-            turnDirection = 0f;
-        }
-    }
-
-	void FixedUpdate()
-	{
-        if (isThrusting)
-        {
-            rb.AddForce(transform.up * moveSpeed);
-        }
-
-        if (turnDirection != 0f)
-        {
-            rb.AddTorque(turnDirection * turnSpeed);
-        }
-	}
 }
